c13c046380a7c6cf51e5d8e8790b96a29a6b262b
Ltphuong123 <101852175+Ltphuong123@users.noreply.github.com>
2025-12-04 11:47:25 +0700

    aa

--- pages/content/vocabulary/components/VocabularyToolbar.tsx
+++ pages/content/vocabulary/components/VocabularyToolbar.tsx
@@ -113,7 +113,7 @@ const VocabularyToolbar: React.FC<VocabularyToolbarProps> = ({
                             onClick={onAddByLevel}
                             className="flex items-center justify-center h-10 px-4 whitespace-nowrap bg-green-600 text-white rounded-lg font-medium text-sm hover:bg-green-700 transition"
                         >
-                            <PlusIcon className="w-5 h-5 mr-2" /> Thêm theo cấp độ
+                            <PlusIcon className="w-5 h-5 mr-2" /> Thêm từ vào sổ tay
                         </button>
                     )}
                 </div>
